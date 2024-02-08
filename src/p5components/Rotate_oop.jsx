2f7fef9ec561810183dc0f6d5643935b42d347d9
nuo <49533950+nuoxoxo@users.noreply.github.com>
2024-02-08 17:11:57 +0100

    rotate_oop/fixed . revert : switching colors green&gold

--- src/p5components/Rotate_oop.jsx
+++ src/p5components/Rotate_oop.jsx
@@ -14,7 +14,7 @@ const Rotate_oop = () => {
       this.size = size
     }
 
-    _rotate( p5 ) { // passing p5 instance HERE, let p5.func get through
+    _rotate(p5) { // passing p5 instance HERE, let p5.func get through
       p5.push()
       p5.translate(this.c, this.r)
       let angle = p5.atan2(p5.mouseY - this.r, p5.mouseX - this.c)
@@ -27,8 +27,6 @@ const Rotate_oop = () => {
   let unit_size = Usize
   let things = []
   let R, C // used later in draw
-
-  let RandomColor
 
   const setup = (p5, canvasParentRef) => {
 
@@ -53,19 +51,11 @@ const Rotate_oop = () => {
     */
 
     p5.createCanvas(800, 800).parent(canvasParentRef)
-
-    const Bright_gold = p5.color(250,215,0,100)
-    const Bright_green = p5.color('#AAFF00')
-    const Colors = [
-      Bright_gold,
-      Bright_green
-    ]
-    RandomColor = Colors[Math.floor(Math.random() * Colors.length)]
     
     p5.rectMode(p5.CENTER)
     p5.angleMode(p5.DEGREES)
     p5.noStroke()
-    
+    const Bright_gold = p5.color(250,215,0,100)
     R = p5.height / unit_size
     C = p5.width / unit_size
     let r = -1
@@ -74,7 +64,7 @@ const Rotate_oop = () => {
       let c = -1
       things[r] = []
       while (++c < C) {
-        p5.fill( RandomColor )
+        p5.fill(Bright_gold)
         things[r][c] = new Something(
           unit_size / 2 + r * unit_size,
           unit_size / 2 + c * unit_size,
@@ -84,7 +74,7 @@ const Rotate_oop = () => {
     }
   }
 
-  const draw = ( p5 ) => {
+  const draw = (p5) => {
     p5.background(0)
     let r = -1, c
     while (++r < R) {
